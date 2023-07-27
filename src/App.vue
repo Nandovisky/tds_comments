c3e8428c030f9ac0b0cc8bf38db00ed7778dfe99
Nandovisky <nando.fiedler@gmail.com>
2023-07-27 14:26:05 -0300

    bruh

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<CommentSection />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import CommentSection from './components/CommentSection'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CommentSection
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
